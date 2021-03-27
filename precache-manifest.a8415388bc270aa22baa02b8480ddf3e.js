1d9edd8f4674750d89b181dcd34e827caeefac9e
Rohit Ranjan <rohitranjan1612@gmail.com>
2021-03-27 23:16:00 +0530

    first commit

---
+++ precache-manifest.a8415388bc270aa22baa02b8480ddf3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2616388b3b750ea1facb24452223333",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d25a2a46f0f4bdbed963",
+    "url": "/static/css/2.aaca35d5.chunk.css"
+  },
+  {
+    "revision": "d5bedf7db68c00189c6e",
+    "url": "/static/css/main.35f3daff.chunk.css"
+  },
+  {
+    "revision": "d25a2a46f0f4bdbed963",
+    "url": "/static/js/2.5ea9bf98.chunk.js"
+  },
+  {
+    "revision": "d5bedf7db68c00189c6e",
+    "url": "/static/js/main.ac337378.chunk.js"
+  },
+  {
+    "revision": "c8101b9a797bfe4fd0f7",
+    "url": "/static/js/runtime~main.d26807ed.js"
+  },
+  {
+    "revision": "338d687536eb82f0d1bc47e911188b62",
+    "url": "/static/media/avatar.338d6875.png"
+  },
+  {
+    "revision": "c738cfe520bc366ccf7682418b588107",
+    "url": "/static/media/name.c738cfe5.png"
+  }
+]);
