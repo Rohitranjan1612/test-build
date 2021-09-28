81a607a464fbfbfc9c419ac4fc0865aa0ad891f1
Rohit Ranjan <rohitranjan1612@gmail.com>
2021-09-28 11:18:08 +0530

    build 5

---
+++ precache-manifest.5b641c53cdea8d22d37f52bb48f0c6f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e8681a34ea7bc27ce6eaf860ad9bb5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ae7552f0d152e7fce0a",
+    "url": "/static/css/2.ae1e9500.chunk.css"
+  },
+  {
+    "revision": "8825d81777ba4cf19499",
+    "url": "/static/css/main.35f3daff.chunk.css"
+  },
+  {
+    "revision": "9ae7552f0d152e7fce0a",
+    "url": "/static/js/2.1f4118e0.chunk.js"
+  },
+  {
+    "revision": "8825d81777ba4cf19499",
+    "url": "/static/js/main.1af8d221.chunk.js"
+  },
+  {
+    "revision": "c8101b9a797bfe4fd0f7",
+    "url": "/static/js/runtime~main.d26807ed.js"
+  },
+  {
+    "revision": "338d687536eb82f0d1bc47e911188b62",
+    "url": "/static/media/avatar.338d6875.png"
+  },
+  {
+    "revision": "c738cfe520bc366ccf7682418b588107",
+    "url": "/static/media/name.c738cfe5.png"
+  }
+]);
